64dc9beddb57d7c245f5842771c8d7921bdaae4d
AleksFulmanski <47895031+AleksFulmanski@users.noreply.github.com>
2019-02-22 16:40:27 +0100

    Version initiale

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>03 Site Aleksandra</title>
+
+ <link rel="shortcut icon" href="http://php.net/favicon.ico">
+  <link rel="stylesheet" type="text/css" href="site3.css">
+ </head>
+ <body>
+ <h1>I LOVE CODE</h1>
+ <h2>I LOVE CODE TOO</h2>
+<p>Littering a dark and dreary road lay the past relics of browser-specific tags, incompatible DOMs, broken CSS support, and abandoned browsers.
+
+We must clear the mind of the past. Web enlightenment has been achieved thanks to the tireless efforts of folk like the W3C, WaSP, and the major browser creators.
+
+The CSS Zen Garden invites you to relax and meditate on the important lessons of the masters. Begin to see with clarity. Learn to use the time-honored techniques in new and invigorating fashion. Become one with the web.</p>
+ </body>
+ </html>
